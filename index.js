9ac624dd1047b391f5783414c3768ca7ec4e331b
shuseelmaharjan <maharzanshuseel10@gmail.com>
2025-01-05 17:59:44 +0545

    updated socket for chatapp

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { cors, corsOptions } = require("./middleware/cors");
 const path = require("path");
 const fs = require("fs");
 const csrf = require("csurf");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const cookieParser = require("cookie-parser");
 
@@ -48,6 +50,23 @@ app.use("/api", shopRoutes);
 
 const siteRoutes = require("./routes/siteRoute");
 app.use("/api/v1", siteRoutes);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+app.set("socketio", io);
 
 const chatRoute = require("./routes/chatRoutes");
 app.use("/api/v1", chatRoute);
